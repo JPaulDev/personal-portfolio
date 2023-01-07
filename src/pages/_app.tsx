34ca6c9e1baae90eb62c2560b1a2969f1a997af1
JonPaulCode <jwatson88@live.co.uk>
2023-01-07 15:56:17 +0000

    Add fonts to the project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,53 @@
+import localFont from '@next/font/local';
 import '@styles/globals.css';
 import type { AppProps } from 'next/app';
 
+const calibre = localFont({
+  src: [
+    {
+      path: './fonts/Calibre-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Calibre-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Calibre-SemiBold.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-calibre',
+});
+
+const sfMono = localFont({
+  src: [
+    {
+      path: './fonts/SFMono-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/SFMono-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './fonts/SFMono-SemiBold.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-sf-mono',
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={`${sfMono.variable} ${calibre.variable} font-sans`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires, import/no-extraneous-dependencies
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
@@ -6,7 +9,12 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-calibre)', ...fontFamily.sans],
+        mono: ['var(--font-sf-mono)', ...fontFamily.mono],
+      },
+    },
   },
   plugins: [],
 };

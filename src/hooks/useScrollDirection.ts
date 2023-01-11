a65bdbb7ad5a58822e6ac2bec4c310e96ac37edd
JonPaulCode <jwatson88@live.co.uk>
2023-01-11 16:54:48 +0000

    Add return type to useScrollDirection hook

--- src/hooks/useScrollDirection.ts
+++ src/hooks/useScrollDirection.ts
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from 'react';
 
 const THRESHOLD = 0;
 
-export default function useScrollDirection() {
+export default function useScrollDirection(): 'up' | 'down' {
   const [scrollDirection, setScrollDirection] = useState<'up' | 'down'>('up');
   const blocking = useRef(false);
   const prevScrollY = useRef(0);
